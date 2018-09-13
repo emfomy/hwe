eb2f23d5af78585726061ff7c071e5f23d07a6da
Mu Yang <emfomy@gmail.com>
2018-09-13 11:44:12 +0800

    Update definition.

---
+++ src/def.hxx
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// \file       src/skipgram.cxx
+/// \brief      The definitions.
+///
+/// \author     Mu Yang <<emfomy@gmail.com>>
+/// \author     Fann Jhih-Sheng <<fann1993814@gmail.com>>
+/// \copyright  Copyright (c) 2017-2018 Mu Yang & Fann Jhih-Sheng. All rights reserved.
+///
+
+#ifndef HWE_DEF_HXX_
+#define HWE_DEF_HXX_
+
+#include <cassert>
+#include <string>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// The HWE namespace
+///
+namespace hwe {
+
+using real_t   = float;
+using index_t  = long long;
+using string_t = std::string;
+
+extern index_t debug_mode;
+
+}  // namespace hwe
+
+#endif  // HWE_DEF_HXX_
